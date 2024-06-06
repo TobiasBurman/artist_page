ebf11fb517c30f2b790d1a22dd39b834cb4e997f
TobiasBurman <113107721+TobiasBurman@users.noreply.github.com>
2024-06-06 14:46:44 +0200

    start-with-header-section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import cageman from "../assets/cageman_white.png";
+import { GlobalStyles, Grid, Link } from "@mui/material";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <>
+      <GlobalStyles
+        styles={{
+          body: { margin: 0, padding: 0, maxWidth: 0 },
+          html: { margin: 0, padding: 0 },
+        }}
+      />
+      <Grid
+        container
+        sx={{ width: "100%" }}
+        // justifyContent={"space-around"}
+        alignItems={"center"}
+      >
+        <Grid item xs={10} container alignItems={"center"}>
+          <img src={cageman} width={200} alt="Logo" />
+        </Grid>
+        <Grid item xs={2} container>
+          <Link href="#" sx={{ margin: 1 }}>
+            Home
+          </Link>
+          <Link href="#" sx={{ margin: 1 }}>
+            Listen
+          </Link>
+          <Link href="#" sx={{ margin: 1 }}>
+            Contact
+          </Link>
+        </Grid>
+      </Grid>
+    </>
+  );
 }
 
-export default Header
+export default Header;
--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
 function LandingPage() {
+
   return (
-    <div>LandingPage</div>
+    <>
+    </>
   )
 }
 
