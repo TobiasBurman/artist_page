2b7662406a3be47d6a44cd78172fe28214f4f7bc
TobiasBurman <113107721+TobiasBurman@users.noreply.github.com>
2024-06-06 13:25:12 +0200

    adjusted-color-to-the-website

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Container } from "@mui/material";
+import Header from "./components/Header";
+import LandingPage from "./components/LandingPage";
+import MyContent from "./components/MyContent";
+import ContactInfo from "./components/ContactInfo";
+import { GlobalStyles } from "@mui/material";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <GlobalStyles
+        styles={{
+          body: {
+            margin: 0,
+            padding: 0,
+            minHeight: "100vh",
+            background: "linear-gradient(to bottom, #010101, #1d263d)"
+          },
+          html: { margin: 0, padding: 0, height: "100%" },
+        }}
+      />
+
+      <Container sx={{ width: "100vw" }}>
+        <Header />
+        <LandingPage />
+        <MyContent />
+        <ContactInfo />
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
